4cc8e68d0a6dd74f9efd9dbc964bd7e4575a17ff
Bskasan <b.kasan@yandex.com>
2022-12-19 17:21:05 +0200

    Nested Loops added

---
+++ JavaScript/JS_04_Loops/Loops07_NestedLoops/loop07.js
@@ -0,0 +1,8 @@
+console.log("-------------------------");
+
+for (let i = 0; i <= 5; i++) {
+  for (let j = 0; j <= 5; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+  console.log("-------------------");
+}
