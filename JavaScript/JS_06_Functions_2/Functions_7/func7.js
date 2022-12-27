a35b21386cab93439b350cf2d0c4375d75cc50a5
Bskasan <b.kasan@yandex.com>
2022-12-27 19:32:45 +0200

    exercises finished

--- JavaScript/JS_06_Functions_2/Functions_7/func7.js
+++ JavaScript/JS_06_Functions_2/Functions_7/func7.js
@@ -9,9 +9,18 @@ function convertCurrency(amount, rate){
 
 // Arrow Function
 const convertCurrency2 = (amount, rate) => amount * rate;
+//const convertCurrency2 = (amount, rate) => {return amount * rate};
 
 console.log(convertCurrency(1000, 1.5));
 console.log(convertCurrency2(1000, 1.5));
 
 console.log('--------------');
 console.log('Exercise - 02');
+
+function numChar(str, letter){
+    let result = str.split('letter').length;
+    return result;
+}
+
+console.log(numChar('Clarusway', 'a'))
+
