0dfceab1a9632aab5ce3c85095d799a7a14ee4aa
Bskasan <b.kasan@yandex.com>
2022-12-29 15:36:28 +0200

    JS string practices started and created required documents

---
+++ JavaScript/JS_07_Strings/stringMethods.js
@@ -0,0 +1,31 @@
+console.log(" ***/// STRING METHODS ///*** ");
+
+let str1 = "Bekir";
+let str2 = "Bekir Sadik";
+let str3 = "This is";
+let str5 = str1 + " " + str2;
+
+//Created new String object with this script.
+let str4 = new String("This is a string");
+
+//console.log(typeof str1); // string
+//console.log(typeof str2); // string
+//console.log(typeof str3); // string
+//console.log(typeof str4); // object.
+
+//---------------------------------//
+console.log(str1.length);
+console.log(str4);
+//---------------------------------//
+
+console.log(" *** length Property *** ");
+console.log(" *** *************** *** ");
+
+console.log(str1.length);
+console.log(str2.length);
+console.log(str3.length);
+console.log(str4.length); // Concatenation
+
+console.log(str1.concat(str2)); // Property for Concatenation.
+
+//---------------------------------//
