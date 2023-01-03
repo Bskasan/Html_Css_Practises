a875b2be9b3899febf4212eaa3dc78de7c9f4aa6
Bskasan <b.kasan@yandex.com>
2023-01-03 19:22:01 +0200

    js array second method to define

--- JavaScript/JS_08_Arrays/arrays.js
+++ JavaScript/JS_08_Arrays/arrays.js
@@ -1,5 +1,5 @@
 // * Data Structures
-// * Client (Browser, computer ...) Side or Server Side 
+// * Client (Browser, computer ...) Side or Server Side
 
 // * Client side -> Local storage, Session Storage, Cookies.
 // * Server side => Server, Database
@@ -15,7 +15,7 @@
 
 // ? How to Declare An Array?
 // ? Arrayin verileri heap alaninda tutulur (Sirali olarak, diger dillerde farkli olabilir lakin JavaScripte bu sekilde tutulmaktadir.)
-// ? const persons = ["Bekir", "Mahmut"]; 
+// ? const persons = ["Bekir", "Mahmut"];
 // ? STORED IN HEAD, BUT REFERENCE IN STACK.
 
 // * ---------------------------------------- *//
@@ -24,11 +24,34 @@
 
 console.log("********* ARRAYS ***********");
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(numbers[0]); // ! First Value.
-console.log(numbers[numbers.length - 1]);// ! Last Value.
+console.log(numbers[numbers.length - 1]); // ! Last Value.
+
+// ? 1. Method - Array Literal Method
+const names = ["Emir", "Micheal", "Bekir", "Jonathan"];
+console.log(names);
+
+// ! By typing dot '.', we will access all methods and parameters for Arrays. (Names dizisinin length property'sine ulasiyoruz.)
+console.log(names.length);
+
+const x = 5.234;
+console.log(x.toFixed(2));
+// * primitive
+// * Primitives normally don't have properties. but;
+// * Ancak JS kullanimi kolaylastirmak icin bazi durumlarda primitive verileri bir obje sarmalayicisi ile sarmalar.
+
+console.log(typeof names); // ! Arrays are Objects. JS de array aslinda object dedigimiz base object'den turetilmistir.
+
+// * Empty Array
+const emptyArray = [];
+console.log(emptyArray);
+
+// ? 2. Method - Object Constructor Method
+// ? Creating new object from Array. The first method is faster than this one, so the first method is recommended.
+const codingLangs = new Array("C", "C++", "JS", "Go");
+console.log(codingLangs);
 
 
 
 // * ---------------------------------------- *//
-
