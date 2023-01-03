f6f98e7a64d95011dafd27041eaa0a99263f14aa
Bskasan <b.kasan@yandex.com>
2023-01-03 19:06:23 +0200

    js arrays started

---
+++ JavaScript/JS_08_Arrays/arrays.js
@@ -0,0 +1,34 @@
+// * Data Structures
+// * Client (Browser, computer ...) Side or Server Side 
+
+// * Client side -> Local storage, Session Storage, Cookies.
+// * Server side => Server, Database
+
+// * Array is data structure that help us to store list of values.
+
+// * Primitive -> number, string, boolean, null, undefined... which is store in stack and easily accessible.
+
+// * Non-primitive -> Object -> Array, function which is more complicated than primitives and stored in heap.
+
+// * An array is a special variable, which can hold more than one value.
+// * In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties.
+
+// ? How to Declare An Array?
+// ? Arrayin verileri heap alaninda tutulur (Sirali olarak, diger dillerde farkli olabilir lakin JavaScripte bu sekilde tutulmaktadir.)
+// ? const persons = ["Bekir", "Mahmut"]; 
+// ? STORED IN HEAD, BUT REFERENCE IN STACK.
+
+// * ---------------------------------------- *//
+
+// * Defining An Array
+
+console.log("********* ARRAYS ***********");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(numbers[0]); // ! First Value.
+console.log(numbers[numbers.length - 1]);// ! Last Value.
+
+
+
+// * ---------------------------------------- *//
+
