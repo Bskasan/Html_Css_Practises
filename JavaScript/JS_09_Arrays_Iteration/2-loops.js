8bef8b5afcca47ef7a808fedccaf6ef2e90a27f6
Bskasan <b.kasan@yandex.com>
2023-01-05 20:47:54 +0200

    for-in

--- JavaScript/JS_09_Arrays_Iteration/2-loops.js
+++ JavaScript/JS_09_Arrays_Iteration/2-loops.js
@@ -17,12 +17,68 @@ for (let i = 0; i < grades.length; i++) {
 console.log((sum / grades.length).toFixed(2));
 
 //?-----------------SAMPLE----------------------
-//? GRADES in ortalamisini hesaplayiniz.
+//? arr dizisindeki negatif ve pozitif sayilari
+//? 2 ayri diziye kaydediniz.
+const arr = [-5, 15, 22, -4, 45, 78];
+
+let negatives = [];
+let positives = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 0) {
+    negatives.push(arr[i]);
+  } else {
+    positives.push(arr[i]);
+  }
+}
+
+console.log(negatives);
+console.log(positives);
+console.log(arr);
 
 //* =======================================================
 //*                      FOR-IN LOOP
 //* =======================================================
 
+//*-------------------------------------------------------
+//* SORU: students dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrencileri aramamizi saglayacak ve aranilan ogrenciden
+//* ne kadar sayida bulunuldugunu ana programa dondurecek bir
+//* fonksiyonu yaziniz. Eger aranilan isimde bir ogrenci yok ise
+//* fonksiyon "ogrenci bulunamadi" dondurulmelidir.
+//*--------------------------------------------------------
+
+const students = [
+  "ahmet",
+  "mehmet",
+  "ismet",
+  "ahmet",
+  "can",
+  "mehmet",
+  "cem",
+  "ahmet",
+];
+
+const findStudent = (name) => {
+  let counter = 0;
+
+  for (let i in students) {
+    if (name.toLowerCase() === students[i]) {
+      counter++;
+    }
+  }
+
+  if (counter === 0) {
+    return `${name} cannot be found.`;
+  } else {
+    return `${name} found ${counter} times.`;
+  }
+};
+
+console.log(findStudent("ahmet"));
+console.log(findStudent("feyza"));
+console.log(findStudent("Mehmet"));
+
 //* =======================================================
-//*                      FOR-PF LOOP
+//*                      FOR-OF LOOP
 //* =======================================================
