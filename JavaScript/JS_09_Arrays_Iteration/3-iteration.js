8e7cda8c4031a88e9b6063e1a42739c7508a9021
Bskasan <b.kasan@yandex.com>
2023-01-05 23:52:17 +0200

    foreach 2

--- JavaScript/JS_09_Arrays_Iteration/3-iteration.js
+++ JavaScript/JS_09_Arrays_Iteration/3-iteration.js
@@ -5,12 +5,10 @@
 console.log("************ ITERATION ************");
 
 let students = ["Jonathan", "Henry", "Micheal", "John"];
-let numbers = 
-students.forEach(WriteOnConsole);
+let numbers = students.forEach(WriteOnConsole);
 
-
-function WriteOnConsole(name){
-    console.log(name);
+function WriteOnConsole(name) {
+  console.log(name);
 }
 
 console.log("*********************************");
@@ -20,27 +18,46 @@ console.log("*********************************");
 
 const prices = [100, 250, 50, 89];
 
-prices.forEach(p => console.log(p));
+prices.forEach((p) => console.log(p));
+
+//? Sample: prices dizisindekilerin toplamini konsola yazdiriniz.
+let sum = 0;
+prices.forEach((price) => (sum += price));
+
+console.log("Sum: ", sum);
+console.log(prices.forEach((price) => (sum += price)));
+//! undefined - it doesn't have return.
+//! NOTE: forEach is a void method that has no return.
+
+//? --------------------- Sample ----------------------------
+//? prices dizisindeki her bir ara toplam degerini konsola
+//? bastiriniz.
+
+let total = 0;
+prices.forEach((p, i, arr) => {
+  total += p;
+  console.log(`${i + 1}. iteration : ${total}`);
+
+  arr[i] = Math.trunc(p * 1.1);
+});
+
+console.log(prices);
 
 //* =======================================================
 //*                      MAP METHOD
 //* =======================================================
 
-
 //* =======================================================
 //*                      CHAINING (PIPELINE)
 //* =======================================================
-
 
 //* =======================================================
 //*                      FILTER METHOD
 //* =======================================================
 
-
 //* =======================================================
 //*                      REDUCE METHOD
 //* =======================================================
-
 
 //* =======================================================
 //*                      OTHERS
