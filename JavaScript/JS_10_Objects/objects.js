d8021ea70995f82281250c96321f0e7506c8429c
Bskasan <b.kasan@yandex.com>
2023-01-09 16:45:18 +0200

    2 definind method has finished

--- JavaScript/JS_10_Objects/objects.js
+++ JavaScript/JS_10_Objects/objects.js
@@ -21,20 +21,44 @@ car.brand = "Volvo";
 car.model = "1990";
 car.color = "egean blue";
 
+const car2 = new Object();
+car2.brand = "BMW";
+car2.model = "2020";
+car2.color = "red";
+
 console.log(car);
 console.log(car.brand); //? Accessing with . notation.
-console.log(car["color"]);//? Accessing with square brackets.
+console.log(car["color"]); //? Accessing with square brackets.
 
 //? Square bracket yonteminin en buyuk avantaji key degerini
 //? degisken olarak kullanabilmemizdir.
-
 
 //*------------------------------------------------------------
 //* 2-  By using object constructor (We'll learn with OOP in detail.)
 //*------------------------------------------------------------
 
-function Personal(id, name, salary){
-    this.id = id;
-    this.name = name;
-    this.salary = salary;
+function Personal(id, name, salary) {
+  this.id = id;
+  this.name = name;
+  this.salary = salary;
+  //console.log(this);//? icinde bulundugunuz object i refere eder.
 }
+
+const p1 = new Personal("12345678910", "Jonathan", 40000);
+console.log(p1);
+
+const p2 = new Personal("123455428910", "Micheal", 55000);
+console.log(p2);
+
+const p3 = new Personal("1231235428910", "Bekir"); //! salary = undefined;
+console.log(p3);
+
+console.log(window.Personal);
+
+//window.alert("Merhaba")
+console.log(this);
+//? Global scope da oldugumuzdan dolayi browser penceresinin en temel objesini yani window u gosterir.
+
+//*------------------------------------------------------------
+//* 3-  Object literal ( Most used and recommended)
+//*------------------------------------------------------------
