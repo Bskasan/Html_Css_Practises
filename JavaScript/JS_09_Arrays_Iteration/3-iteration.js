f89322c86d1c40746a16fa19b41c22a156f6d323
Bskasan <b.kasan@yandex.com>
2023-01-09 15:15:59 +0200

    other methods added and practised

--- JavaScript/JS_09_Arrays_Iteration/3-iteration.js
+++ JavaScript/JS_09_Arrays_Iteration/3-iteration.js
@@ -117,3 +117,46 @@ console.log("TOTAL SALARIES : ", sumSalaries); // ? 375.000
 //* =======================================================
 //*                      OTHERS
 //* =======================================================
+
+//* every()
+//*-----------------------------------------------------------
+//? Tum diziyi itere eder ve aldigi callback fonksiyonuna gore
+//? test gerceklestirir.Tum elemanlar icin test basarili ise
+//? true aksi takdirde false deger dondurur.
+
+const ages = [18, 22, 78, 34, 78, 79, 15];
+const check = ages.every((age) => age >= 18);
+check ? console.log("All ages are over 18") : console.log("Sum are under 18");
+
+//* some()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? En az bir eleman icin bile test basarili ise true aksi
+//? takdirde false deger dondurur.
+
+const over80 = ages.some((a) => a >= 80);
+console.log(over80); //? false
+
+//* find(), findLast()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? Kosulu saglayan ilk dizi elemaninin dondurur.
+//? Eger hic bir eleman kosulu saglamazsa undefined dondurur.
+
+//?Ornek: Yasi 30 dan buyuk olan ilk ve son elemani yazdirin
+const ageBigThan30First = ages.find((age) => age >= 30);
+const ageBigThan30Last = ages.findLast((age) => age >= 30);
+console.log(ageBigThan30First, ageBigThan30Last);
+
+console.log(ages.find((age) => age >= 80)); //? undefined
+
+//* findIndex()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? Kosulu saglayan ilk dizi elemaninin indeksini dondurur.
+//? Eger hic bir eleman kosulu saglamazsa -1 dondurur.
+
+//?Ornek: Yasi 30 dan buyuk olan ilk elemanin indexini yazdirin
+
+const foundIndex = ages.findIndex((age) => age > 18);
+console.log(foundIndex); //? 1
