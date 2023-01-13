a5130690b1b85b5ad99b8fab0032d007fe471445
Bskasan <b.kasan@yandex.com>
2023-01-13 18:56:06 +0200

    createNode js

--- JavaScript/JS_13_Dom-Intro-Continue/3-createNode.js
+++ JavaScript/JS_13_Dom-Intro-Continue/3-createNode.js
@@ -1,13 +1,24 @@
-console.log("**** CREATE NODE ****")
+console.log("**** CREATE NODE ****");
 
 //?Yeni bir P elementi olusturalim
 const newPar = document.createElement("p");
 const text = document.createTextNode("This is a p element added by JS");
 newPar.append(text);
-console.log(newPar);
+console.log(newPar.innerHTML);
 
 const h2 = document.querySelector(".item-list h2");
 //console.log(h2.innerText);
 
 h2.after(newPar);
+//h2.before(newPar);
+
+//?
+const myInput = document.getElementById("input"); //? element
+console.log(myInput.value); //? value
+
+//? id, class
+newPar.id = "par";
+newPar.className = "center red";
+
+//? classList
 
