b712228b80dbbf5056c41d7b329c934d6834db5f
Bskasan <b.kasan@yandex.com>
2023-01-13 17:29:40 +0200

    parentElement, closest and children methods

--- JavaScript/JS_13_Dom-Intro-Continue/2-traversingDOM.js
+++ JavaScript/JS_13_Dom-Intro-Continue/2-traversingDOM.js
@@ -1,4 +1,4 @@
-console.log("**** TRAVERSING DOM ****")
+console.log("**** TRAVERSING DOM ****");
 
 //*===========================================
 //*            TRAVERSING DOM
@@ -15,9 +15,27 @@ console.log("**** TRAVERSING DOM ****")
 
 //* 1- Yukari Yonde traverse
 //*----------------------------------------------
+//! parentElement, parentNode
+
+const list = document.querySelector("li");
+console.log(list);
+
+console.log(list.parentElement); //? ul
+console.log(list.parentNode); //? ul
+console.log(list.parentNode.parentNode); //? main
+
+//! closest(): belirtilen elementin parent'larin ilk uygun parent'i secer.
+console.log(list.closest("section")); //? item-list
+console.log(list.closest("body").querySelector("#header h1")); //? h1
 
 //*2- Asagi Yonde traverse
 //*----------------------------------------------
+const addItem = document.querySelector(".add-item");
+
+console.log(addItem.children); //?HTMLCollection(3) [h2, input#input, input#btn, input: input#input, btn: input#btn]
+
+addItem.children[0].style.color = "red"; //? h2
+console.log(addItem.parentNode.parentNode.children[0].children[0]); //? h1
 
 //*3- Yatay Yonde traverse
 //*----------------------------------------------
