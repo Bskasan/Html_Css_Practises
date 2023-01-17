da5f10be31654f40805ffacfb25d2cf72c8550a9
Bskasan <b.kasan@yandex.com>
2023-01-17 15:17:32 +0200

    win, lose and reload the scene added

--- JavaScript/JS_15_Dom-project-example/app.js
+++ JavaScript/JS_15_Dom-project-example/app.js
@@ -17,13 +17,132 @@
 // });
 //?----------------------------------------------
 
+//******* VARIABLES *********//
 const yourChoice = document.getElementById("your-choice"); //? Store our choice
-const select = document.querySelector(".select"); //? to get the div element named select.
-let userSelect;
+const pcChoice = document.getElementById("pc-choice"); //? Store pc choice
+const select = document.querySelector(".select"); //? to get select div
 
+const scoreYou = document.getElementById("you"); //? For our score
+const scorePc = document.getElementById("pc"); //? For pc score
+
+// Modal Selectors
+const resultDiv = document.querySelector(".result-msg");
+const containerEl = document.querySelector(".container");
+const modalEl = document.querySelector(".modal-container");
+const modalBtn = document.querySelector("#modal-ok");
+
+//For result message
+const final = document.getElementById("final");
+
+let userSelect;
+let pcRandom;
+
+//******* EVENT LISTENERS *********//
+
+//*** FOR SELECTING ***/
 select.addEventListener("click", (e) => {
   //console.log(e.target.className);
   //console.log(e.target.getAttribute("alt"));
-  userSelect = e.target.getAttribute("alt");
-  yourChoice.innerHTML = `<img src="./assets/${userSelect}.png"></img>`;
+  if (e.target.getAttribute("alt")) {
+    userSelect = e.target.getAttribute("alt");
+    yourChoice.innerHTML = `<img src="./assets/${userSelect}.png"></img>`;
+    pc();
+  }
 });
+
+//*** FOR MODAL BUTTON ***/
+modalBtn.addEventListener("click", () => {
+  //modalEl.style.display = "none";
+  modalEl.classList.remove("show");
+  window.location.reload();
+});
+
+//******* FUNCTIONS *********//
+const pcArr = ["stone", "paper", "scissor"];
+
+//*** RESULT FUNCTION ***/
+function result() {
+  switch (userSelect) {
+    case "stone":
+      if (pcRandom == "paper") {
+        Lost();
+      } else if (pcRandom == "scissor") {
+        Win();
+      }
+      break;
+    case "paper":
+      if (pcRandom == "scissor") {
+        Lost();
+      } else if (pcRandom == "stone") {
+        Win();
+      }
+      break;
+    case "scissor":
+      if (pcRandom == "stone") {
+        Lost();
+      } else if (pcRandom == "paper") {
+        Win();
+      }
+      break;
+    default:
+      break;
+  }
+
+  //! FOR DRAW CHECK
+  if (userSelect == pcRandom) {
+    Draw();
+  }
+
+  //! CHECK IF WE WIN (!!!FOR RESULT MESSAGE CHECK!!!)
+  if (scoreYou.innerText == "10") {
+    final.innerHTML = `💃 You Win🕺`;
+    document.querySelector(".modal").style.backgroundColor = "#5AB7AC";
+    modalBtn.style.color = "#5AB7AC";
+  }
+
+  if (scorePc.innerText == "10" || scoreYou.innerText == "10") {
+    Modal();
+  }
+}
+//*****************************/
+
+//*** RANDOM FUNCTION ***/
+function pc() {
+  let pcRandomNum = Math.floor(Math.random() * 3);
+  pcRandom = pcArr[pcRandomNum];
+  console.log(pcRandomNum);
+  console.log(pcRandom);
+  pcChoice.innerHTML = `<img src="./assets/${pcRandom}.png"></img>`;
+  result();
+}
+
+//*** LOST FUNCTION ***/
+function Lost() {
+  resultDiv.classList.add("active");
+  resultDiv.innerHTML = "You Lost";
+  containerEl.style.boxShadow = "3px 3px 10px 1px #fb778b";
+  resultDiv.style.backgroundColor = "#fb778b";
+  scorePc.innerText++;
+}
+
+//*** WIN FUNCTION ***/
+function Win() {
+  resultDiv.classList.add("active");
+  resultDiv.innerHTML = "You Win";
+  containerEl.style.boxShadow = "3px 3px 10px 1px #5AB7AC";
+  resultDiv.style.backgroundColor = "#5AB7AC";
+  scoreYou.innerText++;
+}
+
+//*** DRAW FUNCTION ***/
+function Draw() {
+  resultDiv.classList.add("active");
+  resultDiv.innerHTML = "It's a draw";
+  containerEl.style.boxShadow = "3px 3px 10px 1px #FFC538";
+  resultDiv.style.backgroundColor = "#FFC538";
+}
+
+//*** MODAL USAGE FUNCTION ***/
+function Modal() {
+  modalEl.classList.add("show");
+}
