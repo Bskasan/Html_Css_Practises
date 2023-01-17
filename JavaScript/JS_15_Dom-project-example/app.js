f0e20d067d56fde9812e93bcf904e64a90183ec2
Bskasan <b.kasan@yandex.com>
2023-01-17 13:55:15 +0200

    added add event listener to select class div

--- JavaScript/JS_15_Dom-project-example/app.js
+++ JavaScript/JS_15_Dom-project-example/app.js
@@ -1,18 +1,29 @@
-//? Store our choice
-const yourChoice = document.getElementById("your-choice");
+//? Making choice (Wrong way)
+//? ----------- HARD-CODED WAY -----------------
+// const stoneImage = document.querySelector(".stone");
+// const paperImage = document.querySelector(".paper");
+// const scissorImage = document.querySelector(".scissor");
 
-const stoneImage = document.querySelector(".stone");
-const paperImage = document.querySelector(".paper");
-const scissorImage = document.querySelector(".scissor");
+// stoneImage.addEventListener("click", () => {
+//   yourChoice.innerHTML = '<img src="./assets/stone.png"></img>';
+// });
 
-stoneImage.addEventListener("click", () => {
-  yourChoice.innerHTML = '<img src="./assets/tas.png"></img>';
-});
+// paperImage.addEventListener("click", () => {
+//   yourChoice.innerHTML = '<img src="./assets/paper.png"></img>';
+// });
 
-paperImage.addEventListener("click", () => {
-  yourChoice.innerHTML = '<img src="./assets/kagit.png"></img>';
-});
+// scissorImage.addEventListener("click", () => {
+//   yourChoice.innerHTML = '<img src="./assets/scissor.png"></img>';
+// });
+//?----------------------------------------------
 
-scissorImage.addEventListener("click", () => {
-  yourChoice.innerHTML = '<img src="./assets/makas.png"></img>';
+const yourChoice = document.getElementById("your-choice"); //? Store our choice
+const select = document.querySelector(".select"); //? to get the div element named select.
+let userSelect;
+
+select.addEventListener("click", (e) => {
+  //console.log(e.target.className);
+  //console.log(e.target.getAttribute("alt"));
+  userSelect = e.target.getAttribute("alt");
+  yourChoice.innerHTML = `<img src="./assets/${userSelect}.png"></img>`;
 });
