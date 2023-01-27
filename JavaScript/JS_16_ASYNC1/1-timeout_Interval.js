ee10d55c02ca3344376aaa20cc0a0d0f3bb82470
Bskasan <b.kasan@yandex.com>
2023-01-27 14:08:28 +0200

    callback hell

--- JavaScript/JS_16_ASYNC1/1-timeout_Interval.js
+++ JavaScript/JS_16_ASYNC1/1-timeout_Interval.js
@@ -68,3 +68,24 @@ let sec1 = setInterval(() => {
 //* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
 //* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
 // !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+setTimeout(() => {
+    console.log("john:Hi")
+    setTimeout(() => {
+      console.log("Sarah: Hello")
+      setTimeout(() => {
+        console.log("John: How Are you?")
+        setTimeout(() => {
+          console.log("Sarah:Fine and you?")
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
+  
+  //? COZUMLER:
+  //?----------------------------------------------------
+  //* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+  //? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  //* 2- Promise,
+  //! 3- Fetch API (Promise'in basitlestirilmis hali),
+  //! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
