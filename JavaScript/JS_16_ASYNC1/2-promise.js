3cb748a36b90a8f674c77fb1b43e732c76ae43e6
Bskasan <b.kasan@yandex.com>
2023-01-27 14:49:04 +0200

    promise

---
+++ JavaScript/JS_16_ASYNC1/2-promise.js
@@ -0,0 +1,44 @@
+//* =================================================
+//*                2- Promises
+//* =================================================
+
+//? Promise, asenkron bir islemin basariyla ve basarisizlikla bittigini gosteren
+//? ve ayni zamanda basariyla bittiginde sonuc verilerini temsil eden bir nesne yapisidir.
+
+//? SYTNAX
+//?----------
+//* 1- Ilk olarak new Promise() constructor'i ile yeni bir promise nesnesi olusturulur,
+//* 2- constructor'a asil islemin yapilmasini saglayan bir executor fonksiyion verilir.
+//* 3- Executor fonksiyona ise 2 argument gecirilir: resolve ve reject fonksiyonlari
+//* 4- resolve fonksiyonu promise'in basariyla bittiginda, reject ise
+//*    basarisizlikla bittiginde isletilirler.
+
+//? new Promise (
+//?    /* executor */
+//?    function(resolve,reject){
+//?       .......
+//?    }
+//? )
+
+//? Bir Promise asagidaki state(durumlari) icerebilir:
+//* pending: ilk state, fulfilled veya rejected olmayan
+//* fulfilled:islem basariyla tamamlandini gosteren state.
+//* rejected: islemin basarisizlikla tamamlandigini gosteren state
+
+//! Bir promise bir degerler tamamlanabilir yada bir sebeple (hata) iptal edilebilir.
+//! Bu durumlar then() ve catch() metotlari ile yakalanabilir.
+//? then() ve catch() metotlari promise dondururler.
+//? Zincirleme olarak kullanilabilirler.
+
+console.log("PROMISE");
+
+const myPromise = new Promise((resolve, reject) => {
+  const success = Math.floor(Math.random() * 4);
+  const data = { a: 2, b: 2 };
+  if (success) {
+    console.log("Data fetched");
+    resolve(data);
+  } else {
+    reject(new Error("Something went wrong"))
+  }
+});
