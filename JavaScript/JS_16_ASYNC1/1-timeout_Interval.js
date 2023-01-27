f98b75b62d5bd78fb3a106d961c727df866f911e
Bskasan <b.kasan@yandex.com>
2023-01-27 12:48:47 +0200

    async started

---
+++ JavaScript/JS_16_ASYNC1/1-timeout_Interval.js
@@ -0,0 +1,32 @@
+//*==================================================
+//*        1- Senkron-Asenkron Programlama
+//*==================================================
+
+//! Javascript, single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? --------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basriyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
+//? kullanilmasi cok onemlidir.
+
+//* Senkron
+//* ------------------------------------------------
+
+//* Asenkron (setTimeout)
+//*------------------------------------------------
+
+//* Asenkron (setInterval, clearInterval)
+//*------------------------------------------------
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
