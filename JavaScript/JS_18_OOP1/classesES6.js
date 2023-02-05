f15e78f8b2fcf2e34790c0a0e72210ef4dd4c73c
Bskasan <b.kasan@yandex.com>
2023-02-05 13:23:38 +0200

    classes ES6 started

---
+++ JavaScript/JS_18_OOP1/classesES6.js
@@ -0,0 +1,15 @@
+//* ==============================================
+//*      OOP - Classes and Inheritance (ES6)
+//* ===============================================
+
+//? Classes'lar, object (nesne) olsuturmak icin kullanilan sablonlardir.
+//? JS'de Class'lar prototipler uzerine insa edilmistir. Ancak, syntax
+//? ES5'den farklidir. Aslinda, class keyword'u ilk olarak ES6 da
+//? kullanilmistir ancak bu sadece bir syntactical sugar'dir.
+//? JavaScript, class-tabanli bir dil degil, prototype-tabanli bir dildir.
+//? yaygin kullanim class-tabanli oldugu icin syntax'ini O'na benzetmistir.
+
+//? Bir parent class'in degisken ve fonksiyonelliği extends
+//? keyword'u ile child class'a gecmektedir.(INHERITANCE)
+
+console.log("*** ES6 - Classes ***")
