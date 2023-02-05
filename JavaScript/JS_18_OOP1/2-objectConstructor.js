535f6351d16469e5174ceb4bc816add4ddcb6fa2
Bskasan <b.kasan@yandex.com>
2023-02-05 12:17:12 +0200

    object constructor started

---
+++ JavaScript/JS_18_OOP1/2-objectConstructor.js
@@ -0,0 +1,9 @@
+//* =============================================
+//*        OOP - Object Constructor (ES5)
+//* =============================================
+
+//* Javascript, prototype-temelli bir dildir.
+//* Butun JavaScript nesneleri (objects) ozellikleri (properties)
+//* ve metotlari bir prototipten miras olarak almaktadir.
+//* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
+//* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
