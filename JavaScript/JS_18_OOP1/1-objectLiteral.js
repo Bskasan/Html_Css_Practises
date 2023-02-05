cefd3a1010c312f69a9cc046b482e887932fd6de
Bskasan <b.kasan@yandex.com>
2023-02-05 12:14:43 +0200

    object literal method finished

--- JavaScript/JS_18_OOP1/1-objectLiteral.js
+++ JavaScript/JS_18_OOP1/1-objectLiteral.js
@@ -23,4 +23,19 @@ console.log(book1);
 console.log(book1.getSummary());
 
 //? This method inherits from object prototype(Object nesnesinden geldi.)
-console.log(book1.hasOwnProperty("year"));
+console.log(book1.hasOwnProperty("year")); //? true
+
+const book2 = {
+  title: "The Lily of Valley",
+  author: "Honere de Balzac",
+  year: 1889,
+  getSummary: function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book2.getSummary());
+
+//? Object literals yontemi ile fazla sayida yeni objeler olusturmak oldukca zahmetlidir.
+//? Ayrica programcilik yaklasimi acisindan da cok tekrar icerdigi icin uygun degildir (DRY - Dont Repeat Yourself)
+//? Cozum: Object Oriented Programming (ES5 and ES6)
