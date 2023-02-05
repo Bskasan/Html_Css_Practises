e89e00795f0303179136a353f921dfd3253ce9f1
Bskasan <b.kasan@yandex.com>
2023-02-05 12:03:19 +0200

    book 1

--- JavaScript/JS_18_OOP1/1-objectLiteral.js
+++ JavaScript/JS_18_OOP1/1-objectLiteral.js
@@ -3,4 +3,24 @@
 //* =========================================
 
 //* Object Literals
-console.log("**** Object Literals ****")
+console.log("**** Object Literals ****");
+
+//! JS is not class-based object oriented programming language, but is prototype-based programming language.
+//? Java, C++ --> Objects comes from Classes
+//? in JS, we can create an object without defining a class.
+
+const book1 = {
+  title: "The Karamazov Brothers",
+  author: "Dostoevski",
+  year: 1886,
+  getSummary: function () {
+    //? this keyword refers to the object which its connected.
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book1);
+console.log(book1.getSummary());
+
+//? This method inherits from object prototype(Object nesnesinden geldi.)
+console.log(book1.hasOwnProperty("year"));
