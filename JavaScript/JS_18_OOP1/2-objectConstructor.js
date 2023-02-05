ac03a5a4a73b9177b27447de55f3c1f5a8d19e97
Bskasan <b.kasan@yandex.com>
2023-02-05 12:32:07 +0200

    moved getSummary method to property

--- JavaScript/JS_18_OOP1/2-objectConstructor.js
+++ JavaScript/JS_18_OOP1/2-objectConstructor.js
@@ -16,16 +16,27 @@ function Book(title, author, year) {
   this.title = title; //! Property name, key -> title
   this.author = author;
   this.year = year;
-  this.getSummary = function () {
-    return `${this.title} was written by ${this.author} in ${this.year}`;
-  };
+  //this.getSummary = function () {
+  //  return `${this.title} was written by ${this.author} in ${this.year}`;
+  //};
 }
+
+//? We move our function/method to prototype area. We call only one time this method for each object. It's effective for memory and performance.
+//* Ornegin Book nesnesinin tum instance'lari getSummary() fonksiyonunu miras alabilir. Ancak, getSummary() fonksiyonu bellekte sadece bir yer kaplamaktadir.
+
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}`;
+};
+
+console.log(Book.prototype);
 
 //? Creating an Instance of new object from object constructor
 //? It is easier and cheaper way of instancing of many objects from constructor.
 const book1 = new Book("Kasagi", "Omer Seyfeddin", 1920); // ! book1 --> instance.
 console.log(book1);
 console.log(book1.getSummary());
+
+console.log(book1.__proto__);
 
 // console.log(new Date().getFullYear());
 
@@ -37,6 +48,3 @@ console.log(book2.getSummary());
 //! Prototip alaninda bir metot tanimlamanin avantaji bu metot'un
 //! olusan tum instance'larin belleginde yer kaplamamasi ancak tum
 //! instance'larin bu metota ulasabilmesidir.
-
-//* Ornegin Book nesnesinin tum instance'lari getSummary() fonksiyonunu miras alabilir.
-//* Ancak, getSummary() fonksiyonu bellekte sadece bir yer kaplamaktadir.
