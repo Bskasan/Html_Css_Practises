dbd7d01416dba603cbb21c325eda04fae23ba053
Bskasan <b.kasan@yandex.com>
2023-02-05 12:23:36 +0200

    book object constructor

--- JavaScript/JS_18_OOP1/2-objectConstructor.js
+++ JavaScript/JS_18_OOP1/2-objectConstructor.js
@@ -7,3 +7,21 @@
 //* ve metotlari bir prototipten miras olarak almaktadir.
 //* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
 //* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
+
+//? Object Constructor
+
+function Book(title, author, year) {
+  //! Parameter -> title
+
+  this.title = title; //! Property name, key -> title
+  this.author = author;
+  this.year = year;
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+}
+
+//? Creating an Instance of new object from object constructor
+const book1 = new Book("Kasagi", "Omer Seyfeddin", 1920);
+console.log(book1);
+console.log(book1.getSummary());
