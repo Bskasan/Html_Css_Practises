51576f15a94b78f3fee53fa3532b59df22e56983
Bskasan <b.kasan@yandex.com>
2023-02-06 15:47:03 +0200

    documents created oop 2

---
+++ JavaScript/JS_19_OOP2/4-polymorphism.js
@@ -0,0 +1,12 @@
+//* =====================================
+//*      OOP -  Polymorphism(ES6)
+//*=====================================
+
+//* Polymorphism, bir degisken, fonksiyon veya nesnenin çoklu sekiller
+//* alabilmesini tanimlayan bir nesne-yonelimli programlama teknigidir.
+//* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar
+//* ile bilinir.
+
+console.log("private");
+
+
