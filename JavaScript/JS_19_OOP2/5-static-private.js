02cec665503f6efffa308e8e1dcaf1a9b37de4f9
Bskasan <b.kasan@yandex.com>
2023-02-08 11:06:43 +0200

    static private started

---
+++ JavaScript/JS_19_OOP2/5-static-private.js
@@ -0,0 +1,24 @@
+//* ======================================================
+//*        OOP - Static and Private  (ES6)
+//* ======================================================
+
+//? Static degiskenler ve metotlar butun bir class'i ilgilendiren
+//? verileri tutmak veya degistirmek icin elverislidir.
+
+//? Eger nesnelerden bagimsiz sadece ilgili class' a ait bir degiskene
+//? ihtiyac varsa o zaman static degisken kullanmak mantiklidir.
+
+//! Encapsulation OOP'nin temel unsurlarindan birisidir.
+//! Bir class icerisindeki degeri/durumu class disindan dogrudan ve
+//! izinsiz erisimlere kapatmak icin kullanilir.
+
+//! Encapsulation private degiskenler ve private metotlar yardimiyla yapilir.
+//! ES6 ve sornasinda private degisken ve metotlari belirtmek icin
+//! # (hash) kullanilir.
+
+//! Private degiskenler dogrudan erisilemezler. Bunlara erismek icin class
+//! icerisinde tanimlanan public (genel) erisimli getter ve setter metotlar
+//! kullanilir.
+
+//! Private metotlara ise class disirasindan da eriselemez.
+//! Private metotlara ancak class icerisindeki diger metotlar ile erisilebilir.
