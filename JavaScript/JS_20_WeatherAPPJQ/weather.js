aca2566e5943f4184c06dc2683913bb3b190731e
Bskasan <b.kasan@yandex.com>
2023-02-11 16:52:26 +0200

    jquery methods

--- JavaScript/JS_20_WeatherAPPJQ/weather.js
+++ JavaScript/JS_20_WeatherAPPJQ/weather.js
@@ -7,8 +7,8 @@
 
 //* Selectors ==> eq, th, odd, even ( js + jquery)
 
-const formJS = document.querySelectorAll("form");
-console.log(formJS);
+//const formJS = document.querySelectorAll("form");
+//console.log(formJS);
 
 //const formJQ = jQuery("form");
 //console.log(formJQ);
@@ -19,17 +19,43 @@ console.log(formJS);
 const formJQ = $("form").eq(0);
 console.log(formJQ);
 
+const inputJQ = $("input");
+const msgJQ = $(".msg");
+const listJQ = $(".ajax-section .cities");
+
 //* JS load event
-window.addEventListener("load", () => {
-  console.log("loaded");
-});
+//window.addEventListener("load", () => {
+//  console.log("loaded");
+//});
 
 //* JQUERY load event
-$(window).on("load", () => {
-  console.log("loaded");
-});
+//$(window).on("load", () => {
+//console.log("loaded");
+//});
 
 //* JS DOMContentLoaded
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("dom is ready!");
+//document.addEventListener("DOMContentLoaded", () => {
+//  console.log("dom is ready!");
+//});
+
+//* JQ DOMContentLoaded
+//$(document).on("DOMContentLoaded", () => {
+//  console.log("jquery dom is ready");
+//});
+//! another way
+$(document).ready(() => {
+  console.log("jquery dom is ready!!!");
+  localStorage.setItem(
+    "apiKey",
+    EncryptStringAES("4d8fb5b93d4af21d66a2948710284366")
+  );
 });
+
+formJQ.submit((e) => {
+  e.preventDefault();
+  getWeatherDataFromAPI();
+});
+
+const getWeatherDataFromAPI = () => {
+  const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+};
