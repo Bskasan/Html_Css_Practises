8174052a2b375bd1bd0ca87242834accc4083b97
Bskasan <b.kasan@yandex.com>
2023-02-11 17:36:58 +0200

    jquery finished

--- JavaScript/JS_20_WeatherAPPJQ/weather.js
+++ JavaScript/JS_20_WeatherAPPJQ/weather.js
@@ -53,29 +53,118 @@ $(document).ready(() => {
 
 formJQ.submit((e) => {
   e.preventDefault();
-  getWeatherDataFromAPI();
+  getWeatherDataFromApi();
 });
 
-const getWeatherDataFromAPI = async () => {
+const getWeatherDataFromApi = async () => {
   const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
-  const cityNameInput = inputJQ.val(); //? to get the value
+  const cityNameInput = inputJQ.val();
   const units = "metric";
   const lang = "tr";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}}&appid=${apiKey}}&units=${units}&lang=${lang}`;
-
-  //* jquery http request
-  //const response = await $.get(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}&appid=${apiKey}&units=${units}&lang=${lang}`;
+  //jquery http request
+  //const response = await $.get(url).then(...);
   //const response = await $.post(url);
   await $.ajax({
-    type: "GET",
-    url: url,
-    dataType: "json",
-    success: (response) => {
-      console.log(response);
-    },
-    beforeSend: (request) => {},
-    complete: () => {},
-    error: (XMLHttpRequest) => {},
+      type: "GET",
+      url: url,
+      dataType: "json",
+      success: (response) => {
+          console.log(response);
+          //weather card creation
+          const { main, sys, weather, name } = response;
+          const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+          //alternative iconUrl
+          const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+          //js ==> querySelector-All(selector), jquery ==> find(selector)
+          const cityCardList = listJQ.find(".city");
+          console.log(cityCardList);
+          //Array.from == .get()
+          const cityCardListArray = cityCardList.get();
+          console.log(cityCardListArray);
+          if (cityCardListArray.length > 0) {
+              const filteredArray = cityCardListArray.filter(card => $(card).find("span").text() == name);
+              if (filteredArray.length > 0) {
+                  msgJQ.text(`You already know the weather for ${name}, Please search for another city 😉`);
+                  //styling
+                  msgJQ.css({ "color": "red", "text-decoration": "underline" });
+                  setTimeout(() => {
+                      msgJQ.text(``);
+                  }, 3000);
+                  formJQ.trigger("reset");
+                  return;
+              }
+          }
+
+          const createdLi = $("<li></li>");
+          createdLi.addClass("city");
+          //text(), val(), attr(), html()
+          createdLi.html(
+              `<h2 class="city-name" data-name="${name}, ${sys.country}">
+              <span>${name}</span>
+              <sup>${sys.country}</sup>
+          </h2>
+          <div class="city-temp">${Math.round(main.temp)}
+                  <sup>°C</sup>
+          </div>
+          <figure>
+              <img class="city-icon" src="${iconUrlAWS}">
+              <figcaption>${weather[0].description}</figcaption>
+          </figure>`
+          );
+          //append and prepend ==> js + jquery
+          listJQ.prepend(createdLi);
+          //formJQ[0].reset();
+          formJQ.trigger("reset");
+
+          //jquery animate
+          $(".city").click((e) => {
+              //convert js to jquery
+              //jquery chaning
+              $(e.target)
+                  .animate({ left: '50px' })
+                  .animate({ left: '150px' })
+                  .animate({ left: '250px' })
+                  .animate({ left: '150px' })
+                  .animate({ left: '50px' })
+                  .animate({ left: '0px' });
+          });
+
+          //jquery slideup-slidedown
+          $(".city img").click((e) => {
+              //convert js to jquery
+              //jquery chaning
+              $(e.target)
+                  .slideUp(1500)
+                  .slideDown(1500)
+          });
+
+          //jquery slideup-slidedown
+          // $(".city img").click((e) => {
+          //     //convert js to jquery
+          //     //jquery chaning
+          //     $(e.target)
+          //         .hide()
+          // });
+
+
+
+      },
+      beforeSend: (request) => {
+          //add headers
+          //send tokenKey
+      },
+      complete: () => { },
+      error: (XMLHttpRequest) => {
+          //error handling
+          msgJQ.text(`${XMLHttpRequest.status} ${XMLHttpRequest.statusText}`);
+          //styling
+          msgJQ.css({ "color": "red", "text-decoration": "underline" });
+          setTimeout(() => {
+              msgJQ.text(``);
+          }, 3000);
+          formJQ.trigger("reset");
+      }
   });
-  console.log(response);
-};
+}
